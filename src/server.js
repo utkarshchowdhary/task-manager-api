a25a0a8bc1d4c2852fdad8f443c9492b5c301553
Utkarsh Chowdhary <utkarshchowdhary4140@gmail.com>
2021-05-20 18:24:14 +0530

    refactor

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ process.on('uncaughtException', (err) => {
 const app = require('./app')
 
 const server = app.listen(app.get('port'), () => {
-  console.log(`Server running on port ${app.get('port')}..`)
+  console.log(`Server running on port ${app.get('port')}...`)
 })
 
 process.on('unhandledRejection', (err) => {
